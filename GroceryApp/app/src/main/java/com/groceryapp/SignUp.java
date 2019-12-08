04f6c94a31f1529495774ef0071a3b0347dba71d
Bryan C <47258492+realbryan@users.noreply.github.com>
2019-12-08 16:52:00 -0600

    changed the signup Class

--- GroceryApp/app/src/main/java/com/groceryapp/SignUp.java
+++
@@ -1,4 +0,0 @@
-package com.groceryapp;
-
-public class SignUp {
-}
---
+++ GroceryApp/app/src/main/java/com/groceryapp/SignUp.kt
@@ -0,0 +1,7 @@
+package com.groceryapp
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUp : AppCompatActivity(){
+
+}
