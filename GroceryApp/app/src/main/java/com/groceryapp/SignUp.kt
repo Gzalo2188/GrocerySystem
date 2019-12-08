202448ed02ccb76b5179dbcad5dafe403c00c625
Bryan C <47258492+realbryan@users.noreply.github.com>
2019-12-08 17:29:57 -0600

    Added logic to the signup class

--- GroceryApp/app/src/main/java/com/groceryapp/SignUp.kt
+++ GroceryApp/app/src/main/java/com/groceryapp/SignUp.kt
@@ -1,7 +1,31 @@
 package com.groceryapp
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.signup.*
+import android.widget.Toast
 
 class SignUp : AppCompatActivity(){
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup)
+
+        // Registering an account to FireBase
+        registerButton.setOnClickListener{
+            if((password1.text.toString() == password2.text.toString()) && (username.text.toString() != "")){
+                //This is where if the correct info was put in
+                val tempUser : User = User("nezuko-chan", username.text.toString(), password1.text.toString())
+                Toast.makeText(this, "Account Created" , Toast.LENGTH_SHORT).show()
+            } else if(password1.text.toString() != password2.text.toString()){
+                Toast.makeText(this, "Passwords do not match" , Toast.LENGTH_SHORT).show()
+            } else if (username.text.toString() == ""){
+                Toast.makeText(this, "Not a valid UserName" , Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Could not make Account" , Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
 }
